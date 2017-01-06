e5ec308ecbc76261b7c3b499779a39a13891e719
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-01-05 22:13:17 -0500

    Updating controller file

---
+++ src/main/java/com/news/common/controller/HelloController.java
@@ -0,0 +1,20 @@
+/**
+ * created by Sukhvinder Singh on - 5th January 2017
+ * Copyright © 2017 Sukhvinder Singh. All rights reserved.
+ */
+package com.news.common.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+	@RequestMapping(value="/search", method = RequestMethod.GET)
+	public String get() {
+		return "Hello World!!";
+	}
+	
+}
