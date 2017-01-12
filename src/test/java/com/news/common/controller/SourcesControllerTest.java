3675b7fd5154b631e02fe196420312011d2ef3c6
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-01-11 20:35:17 -0500

    adding sample test class  mockito and junit dependencies

---
+++ src/test/java/com/news/common/controller/SourcesControllerTest.java
@@ -0,0 +1,24 @@
+/**
+ * created by Sukhvinder Singh on - 5th January 2017
+ * Copyright © 2017 Sukhvinder Singh. All rights reserved.
+ */
+package com.news.common.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author Sukh
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SourcesControllerTest {
+	
+	@Test
+	public void test(){
+		assertTrue("true", true);
+	}
+
+}
