ad551d4cd79b64a84a3c1465b535461dddec99d8
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-01-15 13:15:24 -0500

    implemented getCategories call for two categories : 1. Technology 2. Top

---
+++ src/main/java/com/news/application/facade/constant/AppConstant.java
@@ -0,0 +1,16 @@
+package com.news.application.facade.constant;
+
+/**
+ * Created by sukh on 2017-01-15.
+ */
+public class AppConstant {
+
+    public static final String TECH = "tech";
+    public static final String TOP = "top";
+
+
+    // Categories full names
+
+    public static final String TECHNOLOGY = "Technology";
+    public static final String TOP_NEWS = "Top-News";
+}
--- src/main/java/com/news/application/facade/mapper/CategoriesMapper.java
+++ src/main/java/com/news/application/facade/mapper/CategoriesMapper.java
@@ -1,50 +1,58 @@
 package com.news.application.facade.mapper;
 
+import com.news.application.facade.constant.AppConstant;
 import com.news.application.facade.dto.CategoriesDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Properties;
+
 /**
  * Created by sukh on 2017-01-13.
  */
 @Component
-@PropertySource("classpath:application.properties")
 public class CategoriesMapper {
 
     @Autowired
-    private Environment env;
+    private Properties properties;
+
+    private List<String> techSources = new ArrayList<>();
+    private List<String> topSources = new ArrayList<>();
 
     public List<CategoriesDto> prepareDto(){
-        //Just to test if its reading properties ot not this will print  "test" in console
-        System.out.println(env.getProperty("hindtimes.url"));
-        //right now preparing a mock response only
-        // once we will decide json format
-        // CategoriesDto needs to be refactored
-        return mockResponse();
+        //creating enumeration for all the properties name
+        Enumeration e = properties.propertyNames();
+
+        while (e.hasMoreElements()) {
+            //getting the key
+            String key = (String) e.nextElement();
+
+            //assign the source to appropriate category
+            if (key.contains(AppConstant.TECH)){
+                prepareSources(techSources, properties.getProperty(key));
+            }else if (key.contains(AppConstant.TOP)){
+                prepareSources(topSources, properties.getProperty(key));
+            }
+        }
+        return response();
     }
 
+    private void prepareSources(List<String> sources, String source){
+        sources.add(source);
+    }
 
-    private  List<CategoriesDto> mockResponse(){
+    private  List<CategoriesDto> response(){
         List<CategoriesDto> categoriesDtoList = new ArrayList<>();
         CategoriesDto dto = new CategoriesDto();
-        dto.setCategoryName("Technology");
-        List<String> sources = new ArrayList<>();
-        sources.add("cbc-tech");
-        sources.add("mashable");
-        sources.add("android-central");
-        dto.setSources(sources);
+        dto.setCategoryName(AppConstant.TECHNOLOGY);
+        dto.setSources(techSources);
 
         CategoriesDto dto2 = new CategoriesDto();
-        dto2.setCategoryName("Top News");
-        List<String> sources2 = new ArrayList<>();
-        sources2.add("cnn");
-        sources2.add("bbc");
-        sources2.add("timesOfIndia");
-        dto2.setSources(sources2);
+        dto2.setCategoryName(AppConstant.TOP_NEWS);
+        dto2.setSources(topSources);
 
         categoriesDtoList.add(dto);
         categoriesDtoList.add(dto2);
