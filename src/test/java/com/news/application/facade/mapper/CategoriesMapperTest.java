e60fa113350ecac3da457bb48e35a64e6013d9e0
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-01-15 23:00:37 -0500

    adding unit testing for so far what we built

---
+++ src/test/java/com/news/application/facade/mapper/CategoriesMapperTest.java
@@ -0,0 +1,58 @@
+package com.news.application.facade.mapper;
+
+import com.news.application.facade.dto.CategoriesDto;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by sukh on 2017-01-15.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CategoriesMapperTest {
+
+    @InjectMocks
+    private CategoriesMapper mapper;
+
+    @Mock
+    private Properties properties;
+
+    private Hashtable<String,String> hashtable;
+
+    @Before
+    public void setup(){
+        when(properties.propertyNames()).thenReturn(prepareEnumration());
+        when(properties.getProperty("tech.cnn")).thenReturn(hashtable.get("tech.cnn"));
+        when(properties.getProperty("top.bbc")).thenReturn(hashtable.get("top.bbc"));
+        when(properties.getProperty("tech.mashable")).thenReturn(hashtable.get("tech.mashable"));
+    }
+
+    @Test
+    public void test(){
+        List<CategoriesDto> response = mapper.prepareDto();
+        Assert.assertEquals("cnn-tech", response.get(0).getSources().get(0));
+        Assert.assertEquals("bbc", response.get(1).getSources().get(0));
+        Assert.assertEquals("mashable", response.get(0).getSources().get(1));
+    }
+
+    private Enumeration prepareEnumration(){
+        hashtable = new Hashtable<>();
+        hashtable.put("tech.cnn", "cnn-tech");
+        hashtable.put("top.bbc", "bbc");
+        hashtable.put("tech.mashable", "mashable");
+        return hashtable.keys();
+    }
+}
