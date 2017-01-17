5f1ae4139755959e17d2a967f6d28db0c21435ab
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-01-16 19:08:08 -0500

    just some comments

--- src/main/java/com/news/architecture/Exceptions/ErrorHandler.java
+++ src/main/java/com/news/architecture/Exceptions/ErrorHandler.java
@@ -4,15 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.support.RequestContext;
-
-import javax.inject.Inject;
-
 
 /**
  * Created by sukh on 2017-01-16.
+ * Generic class to handle all the error thrown in or after controller
  */
-
 @ControllerAdvice
 public class ErrorHandler {
 
