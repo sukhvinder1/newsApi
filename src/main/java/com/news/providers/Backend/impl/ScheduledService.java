60bc98ed6b1f7f1dcb466790a38b3c3d29570908
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-02-03 11:59:59 -0500

    removing unused imports

--- src/main/java/com/news/providers/Backend/impl/ScheduledService.java
+++ src/main/java/com/news/providers/Backend/impl/ScheduledService.java
@@ -3,10 +3,6 @@ package com.news.providers.Backend.impl;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Sukh on 2017-02-03.
