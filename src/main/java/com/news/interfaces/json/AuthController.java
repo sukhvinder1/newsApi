3fac46d491a67956254484ae62a4bbb9fc25a85c
Sukhvinder Singh <sukhvinder.singh@cibc.com>
2017-03-27 14:43:39 -0400

    adding auth controller

---
+++ src/main/java/com/news/interfaces/json/AuthController.java
@@ -0,0 +1,24 @@
+package com.news.interfaces.json;
+
+import com.news.application.facade.dto.CategoriesDto;
+import com.news.architecture.Exceptions.NewsSystemException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by sukhsingh on 2017-03-27.
+ */
+@RestController
+@RequestMapping("/authTest")
+public class AuthController {
+
+    @RequestMapping(value = "/testPing", method = RequestMethod.GET)
+    public ResponseEntity<String> getError(){
+        return new ResponseEntity<String>("OK", HttpStatus.OK);
+    }
+}
