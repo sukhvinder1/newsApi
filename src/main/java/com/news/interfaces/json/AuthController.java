78ea5eeec72e5677291e7ffe934fb19acae41ef4
Sukhvinder Singh <sukhvinder.singh@cibc.com>
2017-03-27 14:52:24 -0400

    adding params

--- src/main/java/com/news/interfaces/json/AuthController.java
+++ src/main/java/com/news/interfaces/json/AuthController.java
@@ -2,10 +2,12 @@ package com.news.interfaces.json;
 
 import com.news.application.facade.dto.CategoriesDto;
 import com.news.architecture.Exceptions.NewsSystemException;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,9 +19,16 @@ import java.util.List;
 @RequestMapping("/authTest")
 public class AuthController {
 
+    Logger logger = Logger.getLogger(AuthController.class);
+
     @RequestMapping(value = "/testPing", method = RequestMethod.GET)
-    public ResponseEntity<String> getError(){
-        System.out.println("inside testping");
+    public ResponseEntity<String> testPing(@RequestParam String response_type,
+                                           @RequestParam String client_id,
+                                           @RequestParam String state){
+
+        logger.info("Response type : " + response_type);
+        logger.info("client_id  : " + client_id);
+        logger.info("state : " + state);
         return new ResponseEntity<String>("OK", HttpStatus.OK);
     }
 }
